b0141588afdafbfb42fa0c4d1400632d50ae0125
manhzeff <phamducmanh6624@gmail.com>
2023-10-04 22:22:52 +0700

    push trigger_ci_cd

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "mtr050-minh-nhat-phan-bucket3"
+  bucket = "manhzeffbucket1"
 
   tags = {
     Name        = "My bucket"
@@ -21,48 +21,7 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "example1" {
-  bucket = "mtr050-quynh-nhu-bucket-hihi"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
-resource "aws_s3_bucket_ownership_controls" "example1" {
-  bucket = aws_s3_bucket.example1.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
 
-resource "aws_s3_bucket_acl" "example1" {
-  depends_on = [aws_s3_bucket_ownership_controls.example1]
 
-  bucket = aws_s3_bucket.example1.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket" "example2" {
-  bucket = "mtr050-nam-nguyen-hoai"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "example2" {
-  bucket = aws_s3_bucket.example2.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "example2" {
-  depends_on = [aws_s3_bucket_ownership_controls.example2]
-
-  bucket = aws_s3_bucket.example2.id
-  acl    = "private"
-}
